301559f7649c6d157758bb7c15305ba39056e75d
Asrigu <97691762+Asrigu@users.noreply.github.com>
2023-03-21 17:55:57 +0000

    ChangeMat + UI en vr

--- Assets/AngelsRiba_Enviroment/Script/ChangeMaterial.cs
+++ Assets/AngelsRiba_Enviroment/Script/ChangeMaterial.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 public class ChangeMaterial : MonoBehaviour
 {
 
-    [Serializefield]
-    private Material otherMaterial;
-
-    private MeshRenderer _meshRenderer = null;
+    [SerializeField]
+    
+    public Material otherMaterial;
     private Material originalMaterial;
 
+    private MeshRenderer _meshRenderer = null;
+
+    private bool isOriginal;
+   
     void Awake()
     {
-        //obtenemos el coponente de Mesh ENDERER
+        //obtenemos el coponente de Mesh Renderer
         _meshRenderer = GetComponent<MeshRenderer>();
         originalMaterial = _meshRenderer.material;
+        isOriginal = true;
     }
 
-    //public void 
+    //Cambiar el material de manera mas optima
+    public void CambiarMaterial(Material matarialACambiar)
+    {
+        _meshRenderer.material = materialACambiar;
+    }
+
+    //Cambiar de material de manera menos optima pero mas visual
+    //si tiene el material original, cambia por el other
+    public void ChangeToOtherMaterial()
+    {
+        if (isOriginal)
+        {
+            _meshRenderer.material = otherMaterial;
+            isOriginal = false;
+        }
+    }
+    //ai tiene el other material, cambia por el original
+    public void ChangeToOriginalMaterial()
+    {
+        if (!isOriginal)
+        {
+            _meshRenderer.material = originalMaterial;
+            isOriginal = true;
+        }
+    }
 
 }
